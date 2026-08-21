4067055149bd763b0720e12dec975ade68e970a2
pranatgit <35719156+pranatgit@users.noreply.github.com>
2026-08-21 11:32:57 +0530

    created run.sh for pytest

---
+++ pytest/run.sh
@@ -0,0 +1,9 @@
+#cp/home/coder/project/workspace/pytest/tests.py /home/coder/project/workspace/Project/tests.py
+#cd /home/coder/project/workspace/Project
+
+#python3 pytest tests.py-v
+
+/Users/pranat/Documents/Agentic_AI_POC/project/pytest/tests.py /Users/pranat/Documents/Agentic_AI_POC/project/tests.py
+cd /Users/pranat/Documents/Agentic_AI_POC/project
+
+python3 pytest tests.py-v
